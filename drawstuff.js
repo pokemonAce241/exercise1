ca1dd5ef55afdb4b83a2b11b31f330f7b016bbfb
jdbencke <jdbencke@ncsu.edu>
2017-08-21 13:00:53 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -73,8 +73,8 @@ function main() {
  
     // Draw a rectangle with pixels
     var c = new Color(255,255,0,100); // the color at the pixel: black opaque
-    for (var x=20; x<300; x++) 
-        for (var y=50; y<595; y++) {
+    for (var x=20; x<100; x++) 
+        for (var y=50; y<195; y++) {
             drawPixel(imagedata,x,y,c);
             // console.log("draw at " +x+ " " +y);
         }
